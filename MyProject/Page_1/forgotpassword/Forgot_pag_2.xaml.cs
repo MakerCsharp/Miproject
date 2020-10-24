db283bc3d10b509db819271f06e35803bc165c1d
ферденант никита <klin.fray@mail.ru>
2020-10-24 17:41:52 +0300

    -V0.0.1

---
+++ MyProject/Page_1/forgotpassword/Forgot_pag_2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyProject.Page_1.forgotpassword
+{
+    /// <summary>
+    /// Логика взаимодействия для Forgot_pag_2.xaml
+    /// </summary>
+    public partial class Forgot_pag_2 : Page
+    {
+        public Forgot_pag_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyProject/Page_1/page_application/Page_game.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyProject.Page_1.page_application
+{
+    /// <summary>
+    /// Логика взаимодействия для Page_game.xaml
+    /// </summary>
+    public partial class Page_game : Page
+    {
+        public Page_game()
+        {
+            InitializeComponent();
+        }
+    }
+}
